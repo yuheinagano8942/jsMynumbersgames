5e08f8fa9b01a50eeb5cedb257124bf2817c17be
yuhei.nagano <yuheinagano8942@gmail.com>
2019-01-06 07:41:49 +0000

    first commit7

--- main.js
+++ main.js
@@ -6,6 +6,8 @@
   var SIZE = 3;
   var i;
   var currentNum = 0;
+  var panels = [];
+  var panel;
   
   function createPanel(num) {
     var panel;
@@ -21,7 +23,23 @@
     return panel;
   }
   
+  
+  
   for (i = 0; i < SIZE * SIZE; i++) {
-    board.appendChild(createPanel(i));
+    // board.appendChild(createPanel(i));
+    panels.push(createPanel(i));
   }
+  
+  while (panels.length) {
+    panel = panels.splice(Math.floor(Math.random() * panels.length), 1);
+    board.appendChild(panel[0]);
+  }
+  
+  document.getElementById('btn').addEventListener('click', function() {
+    var panels = document.getElementsByClassName('panel');
+    var i;
+    for (i = 0; i < panels.length; i++) {
+      panels[i].className = 'panel';
+    }
+  });
 })();
