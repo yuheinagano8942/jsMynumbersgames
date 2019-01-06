0fb043785061fae45f3ee497b7b3a9b12cb17e2d
yuhei.nagano <yuheinagano8942@gmail.com>
2019-01-06 08:17:53 +0000

    first commit7

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 (function() {
   'use strict';
   
-  var board = document.getElementById('board');
   
-  var SIZE = 3;
-  var i;
+  var SIZE = 2;
+//  var SIZE = 3;
   var currentNum = 0;
-  var panels = [];
-  var panel;
+  var PANEL_WIDTH = 50;
+  var BOARD_PADDING = 10;
+  
   
   function createPanel(num) {
     var panel;
@@ -23,9 +23,20 @@
     return panel;
   }
   
-  
-  
-  for (i = 0; i < SIZE * SIZE; i++) {
+  function initBoard() {
+    var board = document.getElementById('board');
+    var i;
+    var panels = [];
+    var panel;
+    
+    document.getElementById('container').style.width = PANEL_WIDTH * SIZE + BOARD_PADDING * 2 + 'px';
+    
+    
+    while (board.firstChild) {
+      board.removeChild(board.firstChild);
+    }
+    
+    for (i = 0; i < SIZE * SIZE; i++) {
     // board.appendChild(createPanel(i));
     panels.push(createPanel(i));
   }
@@ -34,12 +45,18 @@
     panel = panels.splice(Math.floor(Math.random() * panels.length), 1);
     board.appendChild(panel[0]);
   }
+} 
+   initBoard();
+  
   
   document.getElementById('btn').addEventListener('click', function() {
     var panels = document.getElementsByClassName('panel');
     var i;
+    initBoard();
     for (i = 0; i < panels.length; i++) {
       panels[i].className = 'panel';
     }
+    this.textContent = 'RESTART?';
+    this.className = 'restart';
   });
 })();
